4b492b4c536686df60eb24bdb54a42191c89c020
CS118 <cs118@ubuntu.(none)>
2013-10-15 15:46:20 -0700

    a

---
+++ server.cpp
@@ -0,0 +1,196 @@
+/* A simple server in the internet domain using TCP
+   The port number is passed as an argument 
+   This version runs forever, forking off a separate 
+   process for each connection
+*/
+#include <stdio.h>
+#include <sys/types.h>   // definitions of a number of data types used in socket.h and netinet/in.h
+#include <sys/socket.h>  // definitions of structures needed for sockets, e.g. sockaddr
+#include <netinet/in.h>  // constants and structures needed for internet domain addresses, e.g. sockaddr_in
+#include <stdlib.h>
+#include <strings.h>
+#include <sys/wait.h>	/* for the waitpid() system call */
+#include <signal.h>	/* signal name macros, and the kill() prototype */
+#include <string.h>
+#include <ctime>
+#include <unistd.h>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+using namespace std;
+
+void sigchld_handler(int s)
+{
+    while(waitpid(-1, NULL, WNOHANG) > 0);
+}
+
+void dostuff(int); /* function prototype */
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+     int sockfd, newsockfd, portno, pid;
+     socklen_t clilen;
+     struct sockaddr_in serv_addr, cli_addr;
+     struct sigaction sa;          // for signal SIGCHLD
+
+     if (argc < 2) {
+         fprintf(stderr,"ERROR, no port provided\n");
+         exit(1);
+     }
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+     if (sockfd < 0) 
+        error("ERROR opening socket");
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+     portno = atoi(argv[1]);
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_addr.s_addr = INADDR_ANY;
+     serv_addr.sin_port = htons(portno);
+     
+     if (bind(sockfd, (struct sockaddr *) &serv_addr,
+              sizeof(serv_addr)) < 0) 
+              error("ERROR on binding");
+     
+     listen(sockfd,5);
+     
+     clilen = sizeof(cli_addr);
+     
+     /****** Kill Zombie Processes ******/
+     sa.sa_handler = sigchld_handler; // reap all dead processes
+     sigemptyset(&sa.sa_mask);
+     sa.sa_flags = SA_RESTART;
+     if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+         perror("sigaction");
+         exit(1);
+     }
+     /*********************************/
+     
+     while (1) {
+         newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+         
+         if (newsockfd < 0) 
+             error("ERROR on accept");
+         
+         pid = fork(); //create a new process
+         if (pid < 0)
+             error("ERROR on fork");
+         
+         if (pid == 0)  { // fork() returns a value of 0 to the child process
+             close(sockfd);
+             dostuff(newsockfd);
+             exit(0);
+         }
+         else //returns the process ID of the child process to the parent
+             close(newsockfd); // parent doesn't need this 
+     } /* end of while */
+     return 0; /* we never get here */
+}
+
+/*
+Get content type of file by parsing the path name 
+*/
+string contentType(string path)
+{
+	string file_name;
+	file_name = path.substr(path.find_last_of("/")+1);
+
+	//Check valid file path
+
+	//Check valid file name
+
+	string type = file_name.substr(file_name.find(".")+1);
+	
+	if(type == "html" || type == "htm"){
+		return "text/html";		
+	}else if(type == "gif"){
+		return "image/gif";	
+	}else if(type == "jpg" || type == "jpeg"){
+		return "image/jpeg";
+	}
+
+	//Need to handle case where file type isnt defined
+	return "";
+}
+
+/*
+Parses the http request sent by the client
+*/
+void requestParser(char *request, int sock)
+{
+	int n;
+	string method, path, version, delim;
+	delim = " \n\r\t";
+	method = strtok(request, delim.c_str());
+	path = strtok(NULL, delim.c_str());
+	version = strtok(NULL, delim.c_str());
+	
+	ifstream file(path.c_str());
+	string status, status_msg, content_type, content_length;
+	
+	if(file.is_open()){
+		status_msg = "200 OK";
+	}else{
+		status_msg = "404 Not Found";
+	}
+
+	//Header messages	
+	status = version + " " + status_msg + "\r\n"; 
+	n = write(sock, status.c_str(), status.size());
+
+	time_t timer;
+	time(&timer);
+	string date(ctime(&timer));
+	date = "Date: " + date; 
+	n = write(sock, date.c_str(), date.size());
+
+	content_type = "Content-Type: " + contentType(path) + "\r\n";
+	n = write(sock, content_type.c_str(), content_type.size());
+
+	file.seekg(0, ifstream::end);
+	stringstream ss;
+	ss << file.tellg();
+	content_length = "Content-Length: " + ss.str() + "\r\n";
+	n = write(sock, content_length.c_str(), content_length.size()); 
+	
+	cout << status << date << content_type << content_length << endl;
+
+	//Reset file pos, and also add in a CRLF before the body content	
+	file.clear();
+	file.seekg(0, ifstream::beg);
+	n = write(sock, "\n", 1);
+
+	//Body content
+	string line;
+	if(file.is_open()){
+		while(getline(file, line)){
+			cout << line << endl;
+			line += '\n';
+			n = write(sock, line.c_str(), line.size());
+		}
+		file.close();
+	}
+	return;
+}
+
+/******** DOSTUFF() *********************
+ There is a separate instance of this function 
+ for each connection.  It handles all communication
+ once a connnection has been established.
+ *****************************************/
+void dostuff (int sock)
+{
+   int n;
+   char buffer[256];
+   bzero(buffer,256);
+   n = read(sock,buffer,255);
+   if (n < 0) error("ERROR reading from socket");
+   //printf("Here is the message: %s\n",buffer);
+ //  n = write(sock,"I got your message",18);
+	//string request(buffer);
+	requestParser(buffer, sock);
+   if (n < 0) error("ERROR writing to socket");
+}
